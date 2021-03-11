09d075c713919e82ae7643ba7b95b7261699e5d4
waterme7on <watermelonlee@protonmail.com>
2021-03-12 01:52:16 +0800

    feat: add struct opengauss controller and helper interface (haven't tested out)

--- src/openGauss_operator.go
+++ src/openGauss_operator.go
@@ -20,10 +20,71 @@ import (
 	"time"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/1.5/rest"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
 )
+
+// OpengaussClient is client for operator
+type OpengaussClient struct {
+	Client    *kubernetes.Clientset
+	CrdName   string
+	Namespace string
+	Host      string
+}
+
+// OpengaussManager defines interface for opengauss controller manager
+type OpengaussManager interface {
+	ControllerGetter
+}
+
+// Controller returns a controller
+func (c *OpengaussClient) Controller() ControllerInterface {
+	return newController(c)
+}
+
+func newController(c *OpengaussClient) *controller {
+	return &controller{
+		client:  c.Client.CoreV1().RESTClient(),
+		ns:      &c.Namespace,
+		crdName: &c.CrdName,
+		host:    &c.Host,
+	}
+}
+
+// ControllerGetter returns Controller
+type ControllerGetter interface {
+	Controller() ControllerInterface
+}
+
+// ControllerInterface defines interface for opengauss controller
+type ControllerInterface interface {
+	List(ctx context.Context, opts metav1.ListOptions) (rest.Result, error)
+}
+
+// opengauss controller
+type controller struct {
+	client  rest.Interface
+	ns      *string
+	crdName *string
+	host    *string
+}
+
+func (c *controller) List(ctx context.Context, opts metav1.ListOptions) (res rest.Result, err error) {
+	var timeout time.Duration
+	if opts.TimeoutSeconds != nil {
+		timeout = time.Duration(*opts.TimeoutSeconds) * time.Second
+	}
+	req := c.client.Get()
+	req.RequestURI(*c.host + "/apis/ljt.do/v1")
+	res = req.Namespace(c.ns).
+		Resource(c.crdName).
+		VersionedParams(&opts, scheme.ParameterCodec).
+		Timeout(timeout).
+		Do(ctx)
+	return
+}
 
 func main() {
 	// creates the in-cluster config
@@ -38,6 +99,13 @@ func main() {
 		panic(err.Error())
 	}
 
+	ogClient := OpengaussClient{
+		Client:    clientset,
+		Namespace: "*",
+		CrdName:   "opengauss",
+		Host:      config.Host,
+	}
+
 	for {
 		fmt.Printf("Host:%s, APIPath: %s, BearerToken: , BearerTokenFile: \n", config.Host, config.APIPath)
 		fmt.Printf("type of clientset.CoreV1().RESTClient(): %T\n", clientset.CoreV1().RESTClient())
@@ -50,17 +118,19 @@ func main() {
 		}
 		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
 
-		var timeout time.Duration
-		var res rest.Result
-		opt := metav1.ListOptions{}
-		req := clientset.CoreV1().RESTClient().Get()
-		fmt.Printf("Origin Url: %s\n", req.URL())
-		req.RequestURI(config.Host + "/apis/ljt.do/v1")
-		fmt.Printf("New Url: %s\n", req.URL())
-		// TRY: Successfully get all opengauss cluster info
-		req.Namespace("default").Resource("opengauss").VersionedParams(&opt, scheme.ParameterCodec).Timeout(timeout)
+		// var timeout time.Duration
+		// var res rest.Result
+		// opt := metav1.ListOptions{}
+		// req := clientset.CoreV1().RESTClient().Get()
+		// fmt.Printf("Origin Url: %s\n", req.URL())
+		// req.RequestURI(config.Host + "/apis/ljt.do/v1")
+		// fmt.Printf("New Url: %s\n", req.URL())
+		// // TRY: Successfully get all opengauss cluster info
+		// req.Namespace("default").Resource("opengauss").VersionedParams(&opt, scheme.ParameterCodec).Timeout(timeout)
 
-		res = req.Do(context.TODO())
+		// res = req.Do(context.TODO())
+
+		res, err := ogClient.Controller().List(context.TODO(), metav1.ListOptions{})
 
 		fmt.Printf("%T\n", res)
 		raw, _ := res.Raw()
