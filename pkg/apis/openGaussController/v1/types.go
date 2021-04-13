79f0e1654ce736ec24878ae631e4071ad72c7414
Jintao Li <watermelonlee@protonmail.com>
2021-04-13 17:05:50 +0800

    feat: test out code-generator for OpenGauss (#2)
    usage:
    1. put project to ${GOPATH}/src/github.com/waterme7on
    2. `go vendor` at project root
    3. pkg/apis/openGaussController/v1 should be moved to vendor/github.com/waterme7on/openGauss-controller/pkg/apis/openGaussController/v1
    4. replace code-generator/generate-groups.sh with hack/generate-groups.sh
    5. run hack/update-codegen.sh
    6. then generated code will be in vendor/github.com/waterme7on/openGauss-controller

---
+++ pkg/apis/openGaussController/v1/types.go
@@ -0,0 +1,47 @@
+package v1
+
+import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// OpenGauss is a top-level type
+type OpenGauss struct {
+	metav1.TypeMeta `json:",inline"`
+	// +optional
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+	// +optional
+	Status OpenGaussStatus `json:"status,omitempty"`
+	Spec   OpenGaussSpec   `json:"spec,omitempty"`
+}
+
+type OpenGaussSpec struct {
+	Image           string                         `json:"image"`
+	ImagePullPolicy string                         `json:"imagePullPolicy"`
+	OpenGauss       *OpenGaussClusterConfiguration `json:"opengauss"`
+}
+
+// Define OpenGauss's needs for master and replicas
+type OpenGaussClusterConfiguration struct {
+	Master   int32 `json:"master"`   // Number of Master
+	Replicas int32 `json:"replicas"` // Number of Replicas
+}
+
+// OpenGauss Cluster's status
+type OpenGaussStatus struct {
+	OpenGaussStatus     string `json:"opengaussStatus"`               // OpenGauss if ready or not
+	ReadyMaster         string `json:"readyMaster,omitempty"`         // Ready Master number
+	ReadyReplicas       string `json:"readyReplicas,omitempty"`       // Ready Replicas number
+	MasterStatefulset   string `json:"masterStatefulset,omitempty"`   // name of master statefulset
+	ReplicasStatefulset string `json:"replicasStatefulset,omitempty"` // name of replicas statefulset
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// no client needed for list as it's been created in above
+type OpenGaussList struct {
+	metav1.TypeMeta `json:",inline"`
+	// +optional
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []OpenGauss `json:"items"`
+}
